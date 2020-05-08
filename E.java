4d146b0ff7d713edc0c5bba4ce5a1cf96ef62777
mallikarjunkubasad <kubasadmn@rediffmail.com>
2020-05-08 11:07:46 +0530

    Prajwal Programs

---
+++ E.java
@@ -0,0 +1,13 @@
+package samplePrograms;
+
+public class E {
+
+	public static void main(String[] args) {
+		int a =10;
+		int b = 20;
+		int c = a+b;
+		System.out.println("Sum of two integers is" +c );
+
+	}
+
+}
